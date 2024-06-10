540c45dd9d36b551190aab1fbe4e00b7667c5861
elaineran <eyr4@cornell.edu>
2024-06-10 19:15:54 -0400

    added noise and time stretch funcs to notebook

--- chord_edit.py
+++ chord_edit.py
@@ -1,17 +1,23 @@
+import os
 import librosa
 import numpy as np
+import pandas as pd
 import random
+import soundfile as sf
 
-def time_stretch(data_path, metadata_path, copynum, rate=random.uniform(1.0, 3.0)):
+def time_stretch(data_path, metadata_path, copynum, rate=random.uniform(0.1, 3.0)):
     
     metadata = pd.read_csv(metadata_path)
 
 
     for index, row in metadata.iterrows():
         file_path = os.path.join(data_path, f"{row['Chord']}")
-        y, sr = librosa.load(os.path.join(file_path), f".wav")
-        y_stretch = librosa.effects.time_stretch(y, rate)
-        librosa.output.write_wav(os.path.join(file_path, f"copy {copynum}.wav", y_stretch, sr))
+        y, sr = librosa.load(file_path+".wav")
+        y_stretch = librosa.effects.time_stretch(y, rate=rate)
+        if copynum == 1:
+            sf.write(file_path+" copy.wav", y_stretch, sr)
+        else:
+            sf.write(file_path+" copy "+copynum+".wav", y_stretch, sr)
 
 def add_noise(data_path, metadata_path, copynum):
 
@@ -20,25 +26,25 @@ def add_noise(data_path, metadata_path, copynum):
 
     for index, row in metadata.iterrows():
         file_path = os.path.join(data_path, f"{row['Chord']}")
-        y, sr = librosa.load(os.path.join(file_path), f".wav")
+        y, sr = librosa.load(file_path+".wav")
         noise_amp = 0.005 * np.amax(y)
         noise = noise_amp * np.random.normal(size=y.shape)
         y_noisy = y + noise
-        librosa.output.write_wav(os.path.join(file_path,  f"copy {copynum}.wav", y_noisy, sr))
+        sf.write(os.path.join(file_path, f"copy {copynum}.wav"), y_noisy, sr)
 
-def time_stretch_and_add_noise(data_path, metadata_path, copynum, rate=random.uniform(1.0, 3.0)):
+def time_stretch_and_add_noise(data_path, metadata_path, copynum, rate=random.uniform(0.1, 3.0)):
         
         metadata = pd.read_csv(metadata_path)
     
     
         for index, row in metadata.iterrows():
             file_path = os.path.join(data_path, f"{row['Chord']}")
-            y, sr = librosa.load(os.path.join(file_path), f".wav")
+            y, sr = librosa.load(file_path+".wav")
             noise_amp = 0.005 * np.amax(y)
             noise = noise_amp * np.random.normal(size=y.shape)
             y_noisy = y + noise
-            y_stretch = librosa.effects.time_stretch(y_noisy, rate)
-            librosa.output.write_wav(os.path.join(file_path,  f"copy {copynum}.wav", y_stretch, sr))
+            y_stretch = librosa.effects.time_stretch(y_noisy, rate=rate)
+            sf.write(os.path.join(file_path, f"copy {copynum}.wav"), y_stretch, sr)
 
 
 
